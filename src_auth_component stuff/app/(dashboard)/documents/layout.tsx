be9f50de64651c185240d6bd9f29cbd3e4bf007f
sdey02 <shreyans.sdey02@gmail.com>
2025-03-03 08:58:23 -0700

    Removed auth_component guide

--- src_auth_component stuff/app/(dashboard)/documents/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-export default function DocumentsLayout({ children }: { children: ReactNode }) {
-  return (
-    <div className="container mx-auto py-4">
-      {children}
-    </div>
-  );
-} 
--- src_auth_component stuff/types/backblaze-b2.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'backblaze-b2'; 
